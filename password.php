fef5192d486a18fd4c05952aade3b05590e5fc1f
Kelvin Kuiper <kelvin@dbk.nl>
2016-06-06 13:02:43 +0200

    added password.php

---
+++ password.php
@@ -0,0 +1,14 @@
+
+<?php
+$password = 'dadada';
+echo 'Password: ' . $password . '<br >';
+
+$password_bcrypt = password_hash($password, PASSWORD_BCRYPT);
+echo 'BCRYPT: ' . $password_bcrypt . '<br />';
+
+if(password_verify($password, $password_bcrypt)) {
+	echo "Wachtwoord komt overeen<br />";
+}
+else {
+	echo "Wachtwoord klopt niet";
+}
